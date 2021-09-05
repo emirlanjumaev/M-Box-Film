b10f5e08e1a7b44ffdbad2e92c05cbeabd6e25fd
emirlanjumaev <emirlanjumaev16@gmail.com>
2021-09-04 21:54:27 -1200

    I worked with js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-let 
+let burger = document.querySelector('.header-hanburger');
+let close = document.querySelector('.nav-close');
+
+burger.addEventListener('click', burgerClick);
+close.addEventListener('click', closeClick);
+
+function burgerClick() {
+    let mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.add('open')
+}
+
+function closeClick() {
+    let mobileNav = document.querySelector('.mobile-nav');
+    mobileNav.classList.remove('open')
+
+}
