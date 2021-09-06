962b64c2bede11a0a40558d851b3d317531974af
emirlanjumaev <emirlanjumaev16@gmail.com>
2021-09-05 12:14:41 -1200

    js

--- js/app.js
+++ js/app.js
@@ -6,11 +6,21 @@ close.addEventListener('click', closeClick);
 
 function burgerClick() {
     let mobileNav = document.querySelector('.mobile-nav');
-    mobileNav.classList.add('open')
+    mobileNav.classList.toggle('open');
+
+    if (mobileNav.classList.contains('open')) {
+        mobileNav.style.height += '100vh';
+    } else {
+        mobileNav.removeAttribute('style')
+    }
 }
 
 function closeClick() {
     let mobileNav = document.querySelector('.mobile-nav');
-    mobileNav.classList.remove('open')
+    mobileNav.classList.toggle('open');
+
+    if (mobileNav.classList.contains('open')) {
+        burgerClick()
+    }
 
 }
